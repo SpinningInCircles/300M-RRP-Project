efb90e86576826d7b7681e2a2e9be9c20faa9889
Samantha Tweh <twehsamantha@gmail.com>
2024-05-14 20:51:35 -0500

    ouch

--- 300m-rrp/web_project/rrp/models.py
+++ 300m-rrp/web_project/rrp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#Models
+class Runner(models.Model):
+    firstName = models.CharField(max_length = 255)
+    lastName = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.firstName + self.lastName
+
+class Run(models.Model):
+    runner = models.ForeignKey(Runner,
+                               on_delete = models.CASCADE)
+    date = models.DateTimeField()
+    distance = models.FloatField()
+    time = models.DurationField()
